2e73947684f5fb2d29a827d2fe5f16ad8c34fdc3
Shay Alexander <shaytopazehair@gmail.com>
2018-01-18 17:02:14 +0000

    Refactored code

--- countLetters.js
+++ countLetters.js
@@ -1,35 +1,26 @@
 function countLetters(inputString) {
-var intermediateString = inputString.join(""); //split into an array of characters
-var inputArray = intermediateString.split("");
-var outputObject = {};
-var letterStore = []; //stores an array of the keys of outputObject
 
-for (var i = 0; i < inputArray.length; i++){
-  if(isLetterInside(inputArray[i], letterStore)) {
-    outputObject[inputArray[i]] += 1;
-  }
-  // if letter has been encountered before
-  else {
-    outputObject[inputArray[i]] = 1
+  var intermediateString = inputString.replace(/ /g, ''); //removes spaces lighthouselabs
+  var inputArray = intermediateString.split(""); //split individual characters into array
+  var outputObject = {};
+  // var letterStore = []; //stores an array of the keys of outputObject`
+  for (var i = 0; i < inputArray.length; i++){
+
+    var letter = inputArray[i];
+
+    if(outputObject[letter]) {
+      // if letter has not been encountered before
+      outputObject[letter] += 1;
+    } else {
+      outputObject[letter] = 1;
+    }
+
   }
 
-  letterStore = Object.keys(outputObject); //stores an array of the keys of outputObject
+  return outputObject;
 
 }
 
-return outputObject;
+var args = process.argv[2];
 
-  function isLetterInside(letter, array) {
-    for (var i = 0; i < array.length; i++) {
-      if (array[i] == letter) {
-        return true;
-       }
-     }
-     return false;
-  }
-
-}
-
-
-args = process.argv.slice(2);
 console.log(countLetters(args));
